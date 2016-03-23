b574fafc5f3aeea2e3254576624aae9282e000f0
chuanqi305 <yaochuanqi888@163.com>
2016-03-23 21:38:27 +0800

    完成base64编码程序

--- src/base64.c
+++ src/base64.c
@@ -2,7 +2,7 @@ int encode_base64(char *in, int len, char *out, int *outlen)
 {
 	int i = 0, j = 0, s = 0, tmp;
 	while (i<len || (i==len && s!=0)){
-		tmp = (((i>0?in[i-1]:0)<<((4-s)*2)>>2)|(in[i]>>((s+1)*2)))&(0xFF>>2);
+		tmp = (((i>0?in[i-1]:0)<<((4-s)<<1)>>2)|(in[i]>>((s+1)<<1)))&(0xFF>>2);
 		out[j++] = tmp<26?tmp+65:(tmp<52?tmp+71:(tmp<62?tmp-4:(tmp==62?'+':'/')));
 		s = (s+1)&0x03;
 		if (s != 0) i++;
@@ -15,17 +15,16 @@ int encode_base64(char *in, int len, char *out, int *outlen)
 }
 int decode_base64(char *in, int len, char *out, int *outlen)
 {
-	int i = 0, j = 0, s = 0, tmp, tmp1,tmp2;
+	int i = 0, j = 0, s = 0, tmp, hbyte, lbyte, last;
 	*outlen = 0;
-	while (in[i]!=0 && in[i+1]!='='){
+	while (in[i]!=0 && in[i]!='='){
 		tmp = in[i];
-		tmp1 = tmp=='/'?63:(tmp=='+'?62:((tmp>='a'&&tmp<='z')?tmp-'a'+26:((tmp>='A'&&tmp<='Z')?tmp-'A':((tmp>='0'&&tmp<='9')?tmp-'0'+52:-1))));
-		if (tmp1 < 0) return -1;
-		tmp = in[i+1];
-		tmp2 = tmp=='/'?63:(tmp=='+'?62:(tmp>='a'?tmp-'a'+26:(tmp>='A'?tmp-'A':tmp-'0'+52)));
-		out[j++] = (tmp1<<((s+1)*2))|(tmp2>>(2-s)*2);
-		if (s == 2) i++;
-		s = (s+1)%3;
+		hbyte = last;
+		lbyte = tmp=='/'?63:(tmp=='+'?62:((tmp>='a'&&tmp<='z')?tmp-'a'+26:((tmp>='A'&&tmp<='Z')?tmp-'A':((tmp>='0'&&tmp<='9')?tmp-'0'+52:-1))));
+		if (lbyte < 0) return -1;
+		last = lbyte;
+		if(s!=0) out[j++] = (hbyte<<(s<<1))|(lbyte>>((3-s)<<1));
+		s = (s+1)&0x03;
 		i++;
 	}
 	*outlen = j;
